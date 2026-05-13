a627da9efa5bc3dd4d51c7a6bf73145d14687968
azizu06 <ab725492@ucf.edu>
2026-05-13 16:05:05 -0400

    Start Express ESM app setup

---
+++ app.js
@@ -0,0 +1,39 @@
+import "dotenv/config";
+import path from "node:path";
+import express from "express";
+import session from "express-session";
+import { PrismaSessionStore } from "@quixo3/prisma-session-store";
+import { prisma } from "./lib/prisma.js";
+import passport from "./config/passport.js";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.ur);
+const __dirname = path.join(__filename);
+const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(
+  session({
+    cookie: {
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    },
+    secret: process.env.SESSION_PASS,
+    resave: false,
+    saveUninitialized: false,
+    store: new PrismaSessionStore(prisma, {
+      checkPeriod: 2 * 60 * 1000,
+      dbRecordIdIsSessionId: true,
+    }),
+  }),
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use((req, res, next) => {
+  res.locals.user = req.user;
+  next();
+});
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+
+export { app };
